9b05a1659c0b4901b6aafd1e5b5ee4912b5144cc
Toqa Meqdad <toqamok2005@gmail.com>
2026-03-25 22:30:02 +0300

    alldone

--- ex6/ft_garden_summary.py
+++
@@ -1,6 +0,0 @@
-def ft_garden_summary():
-    name = input("Enter garden name: ")
-    plants = input("Enter number of plants: ")
-    print("Garden:", name)
-    print("Plants:", plants)
-    print("Status: Growing well!")
